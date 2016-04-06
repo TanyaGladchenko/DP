5905e7ff33930f6c618464f864fa478e22e24e57
Tatyana Gladchenko <t.i.gladchenko@gmail.com>
2016-04-07 01:39:02 +0300

    new files. create table

---
+++ draught-common/src/main/java/org/draught/common/entity/StateGame.java
@@ -0,0 +1,64 @@
+package org.draught.common.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author Tatyana_Gladchenko
+ *		<p>
+ *  		StateGame transfer object. Represents StateGame table in database
+ *		</p>
+ */
+
+public class StateGame implements Serializable {
+
+	private static final long serialVersionUID = 3715741579304538898L;
+
+	private long id;
+	private String name;
+		
+	@Override
+	public String toString() {
+		return "StateGame [id=" + id + ", name=" + name + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StateGame other = (StateGame) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/by/bsuir/rusdraughtstraining/rus_draughts_training/App.java
@@ -0,0 +1,13 @@
+package by.bsuir.rusdraughtstraining.rus_draughts_training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
